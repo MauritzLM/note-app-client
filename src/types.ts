085224caf86e57856833875506f0c33e74ea3db8
Mauritz Marais <mauritz303@gmail.com>
2025-02-05 14:33:36 +0200

    added types for note object, notes context

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+// note object
+export interface noteObj {
+  id: string,
+  title: string,
+  text: string,
+  isArchived: boolean,
+  tags: string[]
+}
+
+export interface notesState {
+  notes: noteObj[],
+  updateNotes: (arr: noteObj[]) => void
+}
