d6bfa3a0dba08b9315f6ffdd1c8e80a7e5af0879
Mauritz Marais <mauritz303@gmail.com>
2025-02-06 15:37:07 +0200

    component to render notes with selected tag

---
+++ src/components/lists/SelectedTagList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { all_notes, selected_note } from '@/context';
+import { computed } from 'vue';
+
+const { tag = '' } = defineProps<{ tag?: string }>()
+
+const tag_notes = computed(() => {
+  return all_notes.notes.filter(item => item.tags.includes(tag))
+})
+
+</script>
+
+<template>
+  <h2>Notes tagged: {{ tag }}</h2>
+
+  <ul>
+    <li v-for="n in tag_notes" :key="`${n.id}-${n.isArchived}`" @click="selected_note.changeSelected(n)">
+      {{ n.title }}
+      <div>
+        <span v-for="tag in n.tags" :key="`${n.id}-${tag}`">{{ tag }}</span>
+      </div>
+      <!-- display date* -->
+    </li>
+  </ul>
+</template>
