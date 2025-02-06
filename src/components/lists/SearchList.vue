a05cae3796470df8611dac9147398f242f4c059f
Mauritz Marais <mauritz303@gmail.com>
2025-02-06 15:36:28 +0200

    component to render search list

---
+++ src/components/lists/SearchList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { search_notes } from "@/helpers"
+import { all_notes, selected_note } from "@/context"
+import { ref, computed } from "vue"
+
+const search_term = ref('')
+
+const filteredNotes = computed(() => {
+  return search_notes(search_term.value, all_notes.notes)
+})
+
+</script>
+
+<template>
+  <input type="text" v-model="search_term">
+
+  <p>All notes matching {{ search_term }}</p>
+
+  <ul v-if="search_term !== ''">
+    <li v-for="note in filteredNotes" :key="note.id" @click="selected_note.changeSelected(note)">
+      {{ note.title }}
+      <div>
+        <span v-for="tag in note.tags" :key="`${note.id}-${tag}`">{{ tag }}</span>
+      </div>
+      <!-- display date* -->
+    </li>
+  </ul>
+</template>
