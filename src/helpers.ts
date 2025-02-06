8fe2fe5135b69f08029d22dabbe800f47af2e033
Mauritz Marais <mauritz303@gmail.com>
2025-02-06 15:30:30 +0200

    updated if statements to check for empty in search function

---
+++ src/helpers.ts
@@ -0,0 +1,59 @@
+import { type noteObj } from "./types";
+
+// get all tags
+export function get_all_tags(notesArr: noteObj[]): string[] {
+
+  const tags_list: string[] = []
+  // get all unique tags
+  notesArr.forEach(n => {
+    n.tags.forEach(tag => {
+      if (!tags_list.includes(tag))
+        tags_list.push(tag)
+    }) 
+  });
+
+  return tags_list
+}
+
+// search function
+export function search_notes(searchTerm: string , notesArr: noteObj[]): noteObj[] | [] {
+  const sortedList: noteObj[] = []
+  
+  if (!notesArr.length) {
+    return []
+  }
+
+   for (let i = 0; i < notesArr.length; i++) {
+    let count = 0
+    let found = false
+    // check that note is not already in list
+    for (let j = 0; j < sortedList.length; j++) {
+      if (notesArr[i].id === sortedList[j].id) {
+        count++
+        break
+      }
+    }
+
+    // compare search term to title
+    if (notesArr[i].title && notesArr[i].title.toLowerCase().includes(searchTerm)) {
+       found = true    
+    }
+    
+    // search tags
+    for (let k = 0; k < notesArr[i].tags.length; k++) {
+      if (notesArr[i].tags[k] && notesArr[i].tags[k].toLowerCase().includes(searchTerm)) {
+        found = true
+      }
+    }
+    
+    // not in sorted list and found match with title or tag
+    if (count === 0 && found) {
+      sortedList.push(notesArr[i])
+    }
+
+   }    
+
+  return sortedList
+}
+
+// list by tag* -> note.tags includes selected tag
