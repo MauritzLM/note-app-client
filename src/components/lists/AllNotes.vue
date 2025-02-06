efec6455117dfda66b4e0b7e605848fb6c1271a3
Mauritz Marais <mauritz303@gmail.com>
2025-02-06 15:35:11 +0200

    moved all notes list to own file

---
+++ src/components/lists/AllNotes.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { all_notes, selected_note } from '@/context';
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="note in all_notes.notes" :key="note.id" @click="selected_note.changeSelected(note)">
+      <h3>{{ note.title }}</h3>
+      <div>
+        <span v-for="tag in note.tags" :key="tag">{{ tag }}</span>
+      </div>
+      <!-- display date* -->
+    </li>
+  </ul>
+</template>
