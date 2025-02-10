fa3dac74d11011913ba890a4ca882d65b4808fb5
Mauritz Marais <mauritz303@gmail.com>
2025-02-10 16:00:15 +0200

    component to archive note

---
+++ src/components/forms/ArchiveForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { user, selected_note, auth_status } from '@/context'
+
+async function archive_note(event: Event) {
+  try {
+    event.preventDefault()
+
+    const response = await fetch('http://localhost:8000/notes/archive', {
+      method: 'PUT',
+      headers: {
+        'authorization': `bearer ${user.token}`,
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ id: selected_note.note.id })
+    })
+
+    const data = await response.json()
+
+    if (response.status === 401) {
+      auth_status.logout()
+    }
+
+    // close modal / refresh*
+
+    console.log(data)
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <form action="" @submit="archive_note">
+      <h2>Archive note</h2>
+      <p>Are you sure you want to archive this note? You can find it in the Archived Notes section and restore it
+        anytime.</p>
+      <div>
+        <button type="button">Cancel</button>
+        <button>Archive Note</button>
+      </div>
+
+    </form>
+</template>
