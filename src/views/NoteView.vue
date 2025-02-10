f14ac97d0adbdbd714edb44b7ecf7da91335234a
Mauritz Marais <mauritz303@gmail.com>
2025-02-10 16:04:10 +0200

    moved editor to component, included editor props

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -3,20 +3,10 @@ import SearchList from '@/components/lists/SearchList.vue'
 import ArchivedList from '@/components/lists/ArchivedList.vue'
 import AllNotes from '@/components/lists/AllNotes.vue'
 import TagList from '@/components/lists/TagList.vue'
+import NoteEditor from '@/components/NoteEditor.vue'
 import { auth_status, user, all_notes, selected_note } from '../context.ts'
-import { watch, ref, computed } from 'vue'
+import { watch } from 'vue'
 
-// note text
-const note_text = computed({
-  get: () => selected_note.note.text,
-  set: (val) => val
-})
-
-// compute tags*
-
-
-
-const editor = ref(note_text)
 
 async function get_all_notes() {
   try {
@@ -64,17 +54,14 @@ watch(user, get_all_notes, { immediate: true })
 
     <!-- Tag list -->
     <TagList v-if="$route.params.list === 'tags'" />
-
-    <!-- mobile editor buttons -->
+    
     <div class="editor" v-if="selected_note.displayEditor">
-      <div>
-        <button @click="selected_note.changeDisplay(false)">Go back</button>
-        <!-- save, archive, delete buttons* -->
-      </div>
+      
       <!-- editor -->
-
-      <textarea class="" v-model="editor"></textarea>
+      <NoteEditor :title="selected_note.note.title" :note_text="selected_note.note.text" :tags="selected_note.note.tags"/>
     </div>
-
+    
+    <!-- new note button -->
+    <button v-if="!selected_note.displayEditor" @click="selected_note.newNote()">New</button>
   </main>
 </template>
