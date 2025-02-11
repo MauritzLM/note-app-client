50bf6f4c5266ae8cce52fefbd63dbdc42b3a1888
Mauritz Marais <mauritz303@gmail.com>
2025-02-11 15:37:07 +0200

    added watch function to determine auth status

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { ref } from 'vue';
+import { watch } from 'vue';
 import NavComponent from './components/NavComponent.vue';
 import AuthView from './views/AuthView.vue';
-import { auth_status } from './context';
-// auth status
+import { auth_status, user } from './context';
 
+// determine auth status
+ watch(user, () => {
+   if (localStorage.user) {
+    user.setUser(JSON.parse(localStorage.user))
+    auth_status.login()
+   }
 
-// if authenticated fetch user notes
+ }, { immediate: true, once: true })
+
+ // determine themes*
+
 </script>
 
 <template>
-  <NavComponent v-if="auth_status.authenticated"/>
-  <RouterView v-if="auth_status.authenticated"/>
+  <NavComponent v-if="auth_status.authenticated" />
+  <RouterView v-if="auth_status.authenticated" />
 
-  <AuthView v-else/>
+  <AuthView v-else />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
