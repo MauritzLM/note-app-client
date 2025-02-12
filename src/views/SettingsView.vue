98a3b6ebd631c384836b37652ecde9b50d5d8572
Mauritz Marais <mauritz303@gmail.com>
2025-02-12 15:34:10 +0200

    updated conditional rendering added props to forms

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,9 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ColorThemeForm from '@/components/forms/ColorThemeForm.vue';
+import FontThemeForm from '@/components/forms/FontThemeForm.vue';
+import { user, auth_status } from '@/context';
+import ChangePasswordForm from '@/components/forms/ChangePasswordForm.vue';
+
+const current_setting = ref('')
+
+function select_form(v: string) {
+    current_setting.value = v
+}
+
+
+</script>
+
 <template>
   <div class="settings">
-    <h1>This is the settings page</h1>
+    <h1>Settings</h1>
+
+    <div>
+      <button @click="select_form('color')">Color theme</button>
+      <button @click="select_form('font')">Font theme</button>
+      <button @click="select_form('password')">Change password</button>
+      <button @click="auth_status.logout">Logout</button>
+    </div>
+  </div>
+
+  <div :class="current_setting ? 'current-setting' : 'hide'">
+    <button @click="select_form('')">Settings</button>
+    <ColorThemeForm v-if="current_setting === 'color'" :current_theme="user.theme" :key="user.theme"/>
+
+    <FontThemeForm v-if="current_setting === 'font'" :current_font="user.font" :key="user.font"/>
+
+    <ChangePasswordForm v-if="current_setting === 'password'"/>
+
   </div>
 </template>
 
-<style>
+<style scoped>
+.hide {
+    display: none;
+  }
 
+.settings {
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    button {
+      text-align: left;
+    }
+
+  }
+
+  
+}
 </style>
