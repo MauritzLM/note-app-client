c2ff15d8207bd743fc0a8394c4d7786cc0f56d87
Mauritz Marais <mauritz303@gmail.com>
2025-02-12 15:38:54 +0200

    updated function to determine auth status

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,30 @@ import AuthView from './views/AuthView.vue';
 import { auth_status, user } from './context';
 
 // determine auth status
- watch(user, () => {
-   if (localStorage.user) {
-    user.setUser(JSON.parse(localStorage.user))
+// get token from local storage
+// check if token is valid*
+watch(user, () => {
+  if (!auth_status.authenticated && localStorage.user) {
+    user.updateUser(JSON.parse(localStorage.user))
     auth_status.login()
-   }
+  }
 
- }, { immediate: true, once: true })
+  // fetch all notes if not in context*
 
- // determine themes*
+}, { immediate: true, once: true })
+
+// determine theme and font*
+// const sys_theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 
 </script>
 
 <template>
-  <NavComponent v-if="auth_status.authenticated" />
-  <RouterView v-if="auth_status.authenticated" />
+  <div data-theme="" data-font="">
+    <NavComponent v-if="auth_status.authenticated" />
+    <RouterView v-if="auth_status.authenticated" />
 
-  <AuthView v-else />
+    <AuthView v-else />
+  </div>
 </template>
 
 <style scoped></style>
