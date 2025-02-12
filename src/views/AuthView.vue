82fcc8a700454242c64b98b469156a531eb8ef79
Mauritz Marais <mauritz303@gmail.com>
2025-02-12 15:34:52 +0200

    added signup form and conditional rendering

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import LoginForm from '@/components/auth/LoginForm.vue';
+import SignupForm from '@/components/auth/SignupForm.vue';
+import { ref } from 'vue';
 
-// display login / signup / password reset*
+const current_page = ref('login')
+
+function change_page(p: string) {
+  current_page.value = p
+}
+
+// add password reset link and password reset form*
+
 </script>
 
 <template>
-  <LoginForm />
+  <LoginForm :page-toggle="change_page" v-if="current_page === 'login'" />
+  <SignupForm :page-toggle="change_page" v-if="current_page === 'signup'" />
 </template>
