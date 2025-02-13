5442d2c42210db4fd7f12ce21aaccc6364de6c4f
Mauritz Marais <mauritz303@gmail.com>
2025-02-13 14:21:22 +0200

    updated form layout and mobile styles

--- src/components/forms/ColorThemeForm.vue
+++ src/components/forms/ColorThemeForm.vue
@@ -48,24 +48,42 @@ async function update_theme(event: Event) {
 
 <template>
   <form action="" @submit="update_theme">
-    <div class="form-group">
-      <label for="light"><span>Light mode</span>
-        <input type="radio" id="light" name="color-theme" value="light" v-model="picked">
-      </label>
+    <h2>Color Theme</h2>
+    <p>Choose your color theme:</p>
 
-    </div>
-    <div class="form-group">
-      <label for="dark"><span>Dark mode</span>
-        <input type="radio" id="dark" name="color-theme" value="dark" v-model="picked">
-      </label>
-    </div>
-    <div class="form-group">
-      <label for="system"><span>System</span>
-        <input type="radio" id="system" name="color-theme" value="system" v-model="picked">
-      </label>
+    <div class="input-group">
+      <div class="form-group">
+        <label for="light" :class="picked === 'light' ? 'cs-active' : ''">
+          <div>
+            <span>Light mode</span>
+            <span>Pick a clean and classic light theme</span>
+          </div>
+          <input type="radio" id="light" name="color-theme" value="light" v-model="picked">
+        </label>
+      </div>
+
+      <div class="form-group">
+        <label for="dark" :class="picked === 'dark' ? 'cs-active' : ''">
+          <div>
+            <span>Dark mode</span>
+            <span>Select a sleek and modern dark theme</span>
+          </div>
+          <input type="radio" id="dark" name="color-theme" value="dark" v-model="picked">
+        </label>
+      </div>
+
+      <div class="form-group">
+        <label for="system" :class="picked === 'system' ? 'cs-active' : ''">
+          <div>
+            <span>System</span>
+            <span>Adapts to your devices theme</span>
+          </div>
+          <input type="radio" id="system" name="color-theme" value="system" v-model="picked">
+        </label>
+      </div>
     </div>
 
-    <button>Apply Changes</button>
+    <button type="submit" class="btn-primary">Apply Changes</button>
   </form>
 </template>
 
@@ -73,5 +91,45 @@ async function update_theme(event: Event) {
 form {
   display: flex;
   flex-direction: column;
+
+  h2 {
+    margin: 12px 0;
+  }
+
+  .input-group {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px
+  }
+
+  label {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    border: 1px solid var(--borderColor);
+    border-radius: 10px;
+    cursor: pointer;
+
+    span {
+      display: block;
+    }
+
+    input {
+      height: 16px;
+      width: 16px;
+    }
+  }
+
+  .cs-active {
+    background-color: var(--tagBackground);
+  }
+
+  .btn-primary {
+    align-self: end;
+    margin-top: 24px;
+  }
+
 }
 </style>
--- src/components/forms/FontThemeForm.vue
+++ src/components/forms/FontThemeForm.vue
@@ -49,27 +49,40 @@ async function update_font(event: Event) {
   <form action="" @submit="update_font">
     <h2>Font Theme</h2>
     <p>Choose your font theme:</p>
-    <div class="form-group">
-      <label for="sans-serif">
-        <span>Sans-serif</span>
-        <input type="radio" id="sans-serif" name="font-theme" value="sans-serif" v-model="picked">
-      </label>
 
-    </div>
-    <div class="form-group">
-      <label for="serif">
-        <span>Serif</span>
-        <input type="radio" id="serif" name="font-theme" value="serif" v-model="picked">
-      </label>
-    </div>
-    <div class="form-group">
-      <label for="monospace">
-        <span>Monospace</span>
-        <input type="radio" id="monospace" name="font-theme" value="monospace" v-model="picked">
-      </label>
+    <div class="input-group">
+      <div class="form-group">
+        <label for="sans-serif" :class="picked === 'sans-serif' ? 'cs-active' : ''">
+          <div>
+            <span>Sans-serif</span>
+            <span>Clean and modern, easy to read.</span>
+          </div>
+
+          <input type="radio" id="sans-serif" name="font-theme" value="sans-serif" v-model="picked">
+        </label>
+
+      </div>
+      <div class="form-group">
+        <label for="serif" :class="picked === 'serif' ? 'cs-active' : ''">
+          <div>
+            <span>Serif</span>
+            <span>Classic and elegant for a timeless feel.</span>
+          </div>
+          <input type="radio" id="serif" name="font-theme" value="serif" v-model="picked">
+        </label>
+      </div>
+      <div class="form-group">
+        <label for="monospace" :class="picked === 'monospace' ? 'cs-active' : ''">
+          <div>
+            <span>Monospace</span>
+            <span>Code like, great for a technical vibe.</span>
+          </div>
+          <input type="radio" id="monospace" name="font-theme" value="monospace" v-model="picked">
+        </label>
+      </div>
     </div>
 
-    <button>Apply Changes</button>
+    <button type="submit" class="btn-primary">Apply Changes</button>
   </form>
 </template>
 
@@ -77,5 +90,45 @@ async function update_font(event: Event) {
 form {
   display: flex;
   flex-direction: column;
+
+  h2 {
+    margin: 12px 0;
+  }
+
+  .input-group {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px
+  }
+
+  label {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    border: 1px solid var(--borderColor);
+    border-radius: 10px;
+    cursor: pointer;
+
+    span {
+      display: block;
+    }
+
+    input {
+      height: 16px;
+      width: 16px;
+    }
+  }
+
+  .cs-active {
+    background-color: var(--tagBackground);
+  }
+
+  .btn-primary {
+    align-self: end;
+    margin-top: 24px;
+  }
+
 }
 </style>
