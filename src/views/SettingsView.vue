057509dba8227c3b89736a23ace6d26b6dbae3bf
Mauritz Marais <mauritz303@gmail.com>
2025-02-17 13:02:42 +0200

    added svgs

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -20,15 +20,56 @@ function select_form(v: string) {
       <h1>Settings</h1>
 
       <div>
-        <button @click="select_form('color')">Color theme</button>
-        <button @click="select_form('font')">Font theme</button>
-        <button @click="select_form('password')">Change password</button>
-        <button @click="auth_status.logout">Logout</button>
+        <button @click="select_form('color')">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+              d="M12.055 3v1.372m0 15.256V21m9-9h-1.372M4.427 12H3.055m15.364-6.364-.97.97M6.66 17.394l-.97.97m12.728 0-.97-.97M6.66 6.606l-.97-.97" />
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+              d="M12.055 7.805a4.195 4.195 0 1 1 0 8.39 4.195 4.195 0 0 1 0-8.39Z" clip-rule="evenodd" />
+          </svg>
+          <span>Color theme</span>
+        </button>
+        <button @click="select_form('font')">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path fill="#0E121B" fill-rule="evenodd"
+              d="M20.999 10.979H14.63a1 1 0 0 0-1 1v1.13a1 1 0 1 0 2 0v-.13h1.154v4.409h-.39a1 1 0 1 0 0 2h2.84a1 1 0 1 0 0-2h-.45v-4.41h1.214v.13a1 1 0 1 0 2 0v-1.13a1 1 0 0 0-1-1Z"
+              clip-rule="evenodd" />
+            <path fill="#0E121B" fill-rule="evenodd"
+              d="M12.185 17.388H10.29V6.61h4.415v1.25a1 1 0 0 0 2 0V5.61a1 1 0 0 0-1-1H2.999a1 1 0 0 0-1 1v2.25a1 1 0 0 0 2 0V6.61H8.29v10.78H6.517a1 1 0 1 0 0 2h5.668a1 1 0 1 0 0-2Z"
+              clip-rule="evenodd" />
+          </svg>
+          <span>Font theme</span>
+        </button>
+        <button @click="select_form('password')">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8"
+              d="M16.424 9.448V7.3a4.552 4.552 0 0 0-4.551-4.551 4.55 4.55 0 0 0-4.57 4.53v2.168" />
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8"
+              d="M15.683 21.25H8.042a3.792 3.792 0 0 1-3.792-3.792v-4.29a3.792 3.792 0 0 1 3.792-3.791h7.641a3.792 3.792 0 0 1 3.792 3.792v4.289a3.792 3.792 0 0 1-3.792 3.792Z"
+              clip-rule="evenodd" />
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8"
+              d="M11.862 14.203v2.22" />
+          </svg>
+          <span>Change password</span>
+        </button>
+        <button @click="auth_status.logout">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path stroke="#0E121B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8"
+              d="M21 11.998H8.945m12.055 0-2.932-2.934M21 11.998l-2.932 2.936M14.556 8.266V7.251c0-1.56-1.121-2.891-2.651-3.15L6.702 3.046C4.765 2.718 3 4.219 3 6.195v11.61c0 1.976 1.765 3.477 3.702 3.15l5.203-1.057a3.188 3.188 0 0 0 2.65-3.149v-1.014" />
+          </svg>
+          <span>Logout</span>
+        </button>
       </div>
     </div>
 
     <div :class="current_setting ? 'current-setting' : 'hide'">
-      <button @click="select_form('')">Settings</button>
+      <button class="back-btn" @click="select_form('')"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+          fill="none" viewBox="0 0 24 24">
+          <path fill="#000" fill-rule="evenodd" d="M15.75 20.414 7.336 12l8.414-8.414L17.164 5l-7 7 7 7-1.414 1.414Z"
+            clip-rule="evenodd" />
+        </svg>
+        <span>Settings</span>
+      </button>
       <ColorThemeForm v-if="current_setting === 'color'" :current_theme="user.theme" :key="user.theme" />
 
       <FontThemeForm v-if="current_setting === 'font'" :current_font="user.font" :key="user.font" />
@@ -63,6 +104,17 @@ function select_form(v: string) {
       padding: 17px 0;
       font-size: 14px;
       font-weight: 500;
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      color: var(--descriptionTextColor);
+
+      /* svg {
+        path {
+          fill: var(--descriptionTextColor);
+        }
+        
+      } */
 
       &:nth-of-type(3) {
         border-bottom: 1px solid var(--borderColor);
@@ -72,5 +124,13 @@ function select_form(v: string) {
   }
 
 
+}
+
+.current-setting {
+  .back-btn {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
 }
 </style>
