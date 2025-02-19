0e7d13aaa1f53c046339090f6529bc567fac9a2e
Mauritz Marais <mauritz303@gmail.com>
2025-02-19 15:05:38 +0200

    added tests for initial rendering and user selecting an option

---
+++ src/views/__tests__/settings.test.ts
@@ -0,0 +1,30 @@
+import { describe, test, expect } from "vitest";
+import { mount } from '@vue/test-utils'
+import SettingsView from "../SettingsView.vue";
+
+
+describe('test settings component', () => {
+
+
+  test('initial rendering', () => {
+    const wrapper = mount(SettingsView)
+
+    const setting_options = wrapper.findAll('[data-test=option]')
+
+    expect(setting_options).toHaveLength(4)
+    // display: none on current-setting*
+    expect(wrapper.find('[data-test=current-setting]').isVisible()).toBe(false)
+  });
+
+  test('user selects a setting option', async () => {
+    const wrapper = mount(SettingsView)
+
+    const setting_options = wrapper.findAll('[data-test=option]')
+
+    // click color theme button and assert form is visible
+    await setting_options[0].trigger('click')
+
+    expect(wrapper.find('[data-test=form]').isVisible()).toBe(true)
+
+  });
+});
