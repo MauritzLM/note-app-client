3159c3ab5164a71d06b9d7662bd813cf24ef2464
Mauritz Marais <mauritz303@gmail.com>
2025-02-24 12:58:43 +0200

    added tests for rendering, closing modal and form submission

---
+++ src/components/forms/__tests__/deleteform.test.ts
@@ -0,0 +1,55 @@
+
+import { describe, test, expect, vi } from "vitest";
+import { mount } from '@vue/test-utils'
+import DeleteForm from "../DeleteForm.vue";
+
+// mock fetch
+const mockFetch = vi.fn()
+mockFetch.mockReturnValue(Promise.resolve({
+  status: 200,
+  json: () => Promise.resolve({ "details": "note deleted" })
+})
+)
+
+vi.stubGlobal('fetch', mockFetch)
+
+// close modal, submit
+describe('archive form test', () => {
+  test('initial rendering', () => {
+    const wrapper = mount(DeleteForm, {
+      props: {
+        showModal: vi.fn()
+      }
+    });
+
+    expect(wrapper.find('[data-test=form]').exists()).toBe(true)
+  });
+
+  test('clicking cancel button calls showModal function', async () => {
+    const wrapper = mount(DeleteForm, {
+      props: {
+        showModal: vi.fn()
+      }
+    });
+
+    const cancel_btn = wrapper.find('[data-test=cancel]')
+
+    await cancel_btn.trigger('click')
+
+    // showmodal has been called
+    expect(wrapper.props().showModal).toHaveBeenCalled()
+
+  })
+
+  test('submit form', async () => {
+    const wrapper = mount(DeleteForm, {
+      props: {
+        showModal: vi.fn()
+      }
+    });
+
+    await wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.emitted()).toHaveProperty('submit')
+  })
+});
